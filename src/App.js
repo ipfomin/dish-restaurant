ce87b526ed2505b2b4878076bba3da4f00b0cada
ipfomin <harry.phomin@gmail.com>
2020-07-12 01:54:10 +0500

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Restourant } from 'src/pages';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Restourant name="harry" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,1 @@
+export { default as Restourant } from './Restourant/Restourant'
