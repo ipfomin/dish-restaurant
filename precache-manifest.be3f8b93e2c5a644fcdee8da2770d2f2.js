e5dd01a91636378ac51c27b5656ce1406a00a5d7
ipfomin <harry.phomin@gmail.com>
2020-07-14 17:38:20 +0500

    Updates

---
+++ precache-manifest.be3f8b93e2c5a644fcdee8da2770d2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0cb2ba438d5196886155af5672f8f1",
+    "url": "/dish-restaurant/index.html"
+  },
+  {
+    "revision": "01c3b4bb730cc562c0b9",
+    "url": "/dish-restaurant/static/css/main.6dd562b1.chunk.css"
+  },
+  {
+    "revision": "b6f2508a9d36380cda14",
+    "url": "/dish-restaurant/static/js/2.83a530eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dish-restaurant/static/js/2.83a530eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c3b4bb730cc562c0b9",
+    "url": "/dish-restaurant/static/js/main.8ca55660.chunk.js"
+  },
+  {
+    "revision": "441a67dfa193eb5af380",
+    "url": "/dish-restaurant/static/js/runtime-main.b386c8ac.js"
+  }
+]);
